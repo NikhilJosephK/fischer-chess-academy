b1819c8359dd2fac1405bfef84a47a24a7d01377
NikhilJosephK <nikhilnjk@gmail.com>
2025-02-27 16:10:23 +0530

    Refactor social media icons in footer

--- wp-content/themes/fischer/footer.php
+++ wp-content/themes/fischer/footer.php
@@ -13,9 +13,11 @@
             </div>
             <div class="contact">
                 <h3><strong>Contact Us</strong></h3>
-                <div>
-                    <img src="/wp-content/themes/fischer/assets/images/footer/fca-contact-phone-icon.svg" alt="">
-                    <a href="tel:9920254240">9920254240 </a>
+                <div class="social-media">
+                    <a href="https://www.facebook.com/fischerchessindia/"><img src="/wp-content/themes/fischer/assets/images/footer/fishcer-fb.svg" alt="facebook"></a>
+                    <a href="https://www.instagram.com/fischerchessindia/?hl=en"><img src="/wp-content/themes/fischer/assets/images/footer/fischer-instagram.svg" alt="instagram"></a>
+                    <a href="https://wa.me/9920254240"><img src="/wp-content/themes/fischer/assets/images/footer/fischer-whatsapp.svg" alt="whatsapp"></a>
+                    <a href="https://youtu.be/R_ADzLP8pVA?si=dZwa6W5DRwVnubji"><img src="/wp-content/themes/fischer/assets/images/footer/fischer-youtube.svg" alt="youtube"></a>
                 </div>
                 <div>
                     <img src="/wp-content/themes/fischer/assets/images/footer/fca-footer-email-icon.svg" alt="">
